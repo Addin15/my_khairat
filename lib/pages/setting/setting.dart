b3daa891eb18323b392c353458922b44437677a9
aqilgits <aqil.dev@gmail.com>
2022-05-04 10:47:53 +0800

    setting draft

--- lib/pages/setting/setting.dart
+++ lib/pages/setting/setting.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_khairat/main.dart';
-import 'package:my_khairat/pages/complete_profile.dart';
-import 'package:my_khairat/pages/payment/payment.dart';
 import 'package:my_khairat/pages/setting/edit_profile.dart';
 import 'package:my_khairat/styles/app_color.dart';
 
@@ -14,79 +11,88 @@ class Setting extends StatefulWidget {
 
 class _SettingState extends State<Setting> {
   @override
+  int index = 0;
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 200,
-            height: 80,
-            child: Card(
-              color: AppColor.primary,
-              shadowColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditProfile()),
-                  );
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.person,
-                      color: Colors.white,
-                    ),
-                    Text("Profil", style: TextStyle(color: Colors.white)),
-                  ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 80,
+              child: Card(
+                color: AppColor.primary,
+                shadowColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      // MaterialPageRoute(
+                      //     builder: (context) => const EditProfile()),
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation1, animation2) =>
+                            const EditProfile(),
+                        transitionDuration: Duration.zero,
+                        reverseTransitionDuration: Duration.zero,
+                      ),
+                    );
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      Text("Profil", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            width: 200,
-            height: 80,
-            child: Card(
-              color: Colors.redAccent,
-              shadowColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditProfile()),
-                  );
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.logout,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "Keluar",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 200,
+              height: 80,
+              child: Card(
+                color: Colors.redAccent,
+                shadowColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const EditProfile()),
+                    );
+                  },
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.logout,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Keluar",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
