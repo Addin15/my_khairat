110f0ecf645507f7a7d3fdb5d98f9bf5c418a011
aqilgits <aqil.dev@gmail.com>
2022-05-04 09:46:44 +0800

    setting draft

---
+++ lib/pages/setting/edit_profile.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:my_khairat/styles/app_color.dart';
+import 'package:my_khairat/styles/custom_text_field.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({Key? key}) : super(key: key);
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  final TextEditingController _icController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _occupationController = TextEditingController();
+
+  final FocusNode _icFocus = FocusNode();
+  final FocusNode _nameFocus = FocusNode();
+  final FocusNode _addressFocus = FocusNode();
+  final FocusNode _phoneFocus = FocusNode();
+  final FocusNode _occupationFocus = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            // gradient: LinearGradient(
+            //   begin: Alignment.topLeft,
+            //   end: Alignment.bottomCenter,
+            //   colors: [
+            //     AppColor.secondary,
+            //     AppColor.primary,
+            //   ],
+            // ),
+            color: Colors.white54,
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'MyKhairat',
+                style: TextStyle(
+                  color: AppColor.primary,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              CustomTextFormField(
+                hintText: 'Kad Pengenalan',
+                focusNode: _icFocus,
+                controller: _icController,
+              ),
+              const SizedBox(height: 10),
+              CustomTextFormField(
+                hintText: 'Nama',
+                focusNode: _nameFocus,
+                controller: _nameController,
+              ),
+              const SizedBox(height: 10),
+              CustomTextFormField(
+                hintText: 'Alamat',
+                focusNode: _addressFocus,
+                controller: _addressController,
+              ),
+              const SizedBox(height: 10),
+              CustomTextFormField(
+                hintText: 'No Telefon',
+                focusNode: _phoneFocus,
+                controller: _phoneController,
+              ),
+              const SizedBox(height: 10),
+              CustomTextFormField(
+                hintText: 'Pekerjaan',
+                focusNode: _occupationFocus,
+                controller: _occupationController,
+              ),
+              const SizedBox(height: 15),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: AppColor.primary,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 20,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Selesai',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/setting/setting.dart
+++ lib/pages/setting/setting.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_khairat/main.dart';
 import 'package:my_khairat/pages/complete_profile.dart';
 import 'package:my_khairat/pages/payment/payment.dart';
+import 'package:my_khairat/pages/setting/edit_profile.dart';
 import 'package:my_khairat/styles/app_color.dart';
 
 class Setting extends StatefulWidget {
@@ -25,12 +26,15 @@ class _SettingState extends State<Setting> {
             child: Card(
               color: AppColor.primary,
               shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
               child: TextButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const CompleteProfile()),
+                        builder: (context) => const EditProfile()),
                   );
                 },
                 child: Column(
@@ -41,23 +45,28 @@ class _SettingState extends State<Setting> {
                       Icons.person,
                       color: Colors.white,
                     ),
-                    Text("Profile", style: TextStyle(color: Colors.white)),
+                    Text("Profil", style: TextStyle(color: Colors.white)),
                   ],
                 ),
               ),
             ),
           ),
+          const SizedBox(height: 20),
           SizedBox(
             width: 200,
             height: 80,
             child: Card(
               color: Colors.redAccent,
+              shadowColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
               child: TextButton(
                 onPressed: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const CompleteProfile()),
+                        builder: (context) => const EditProfile()),
                   );
                 },
                 child: Column(
@@ -69,7 +78,7 @@ class _SettingState extends State<Setting> {
                       color: Colors.white,
                     ),
                     Text(
-                      "Logout",
+                      "Keluar",
                       style: TextStyle(color: Colors.white),
                     ),
                   ],
